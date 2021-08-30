c5121269ca5ac8979f97b752053b8fd548059317
ahmadmanzoor <forlorn.ma@gmail.com>
2021-08-30 23:15:59 +0000

    added url in frontend

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -4,7 +4,7 @@ export const USDC_DECIMAL_POINTS = 2;
 export const ASSET_URL = 'opennft.dhariyat.com';
 export var BACKEND_URL;
 if (process.env.NODE_ENV === 'development') {
-  BACKEND_URL = 'https://opennft.dhariyat.com/';
+  BACKEND_URL = 'https://opennft.dhariyat.com';
 } else {
-  BACKEND_URL = 'https://opennft.dhariyat.com/';
+  BACKEND_URL = 'https://opennft.dhariyat.com';
 }
